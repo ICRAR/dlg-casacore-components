268c1476246d4797110bf8d9e3d534ce495d7ff6
Callan Gray <cal.j.gray@gmail.com>
2022-05-20 11:38:06 +0800

    print app args

--- dlg_casacore_components/ms.py
+++ dlg_casacore_components/ms.py
@@ -195,7 +195,7 @@ class MSReadApp(BarrierAppDROP):
 
     def run(self):
         named_outputs = self._generate_named_outputs()
-        print(f"arguments: {self.arguments.args}")
+        print(f"appArgs: {self.parameters["applicationArgs"]}")
 
         if len(self.inputs) < 1:
             raise DaliugeException(f"MSReadApp has {len(self.inputs)} input drops but requires at least 1")
